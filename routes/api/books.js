e39a49d92386713bb8c43bffcd08e993ca096079
Alicia Forstner <aliciaf@teamsmt.com>
2020-05-14 12:31:32 -0500

    pushing files

---
+++ routes/api/books.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+
+const booksController = require("../../controllers/bookController");
+
+
+
+// Matches with "/api/books"
+
+router.route("/")
+
+  .get(bookController.findAll)
+
+  .post(bookController.create);
+
+
+
+// Matches with "/api/books/:id"
+
+router
+
+  .route("/:id")
+
+  .get(bookController.findById)
+
+  .put(bookController.update)
+
+  .delete(bookController.remove);
+
+
+
+module.exports = router;
---
+++ routes/api/index.js
@@ -0,0 +1,14 @@
+
+
+const router = require("express").Router();
+
+const bookRoutes = require("./books");
+
+
+
+//Book Routes
+router.use("/books", bookRoutes);
+
+
+
+module.exports = router;
